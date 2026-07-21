8991f6eef7af36cdcfd2be16fec8a308390adde1
Yousif Abdulhafiz <89231113+ysif9@users.noreply.github.com>
2026-07-22 00:54:28 +0300

    Add: python-dict-items - submission-0

---
+++ Python Coding Interviews/python-dict-items/submission-0.py
@@ -0,0 +1,15 @@
+from typing import Dict, List, Tuple
+
+
+def get_dict_items(age_dict: Dict[str, int]) -> List[Tuple[str, int]]:
+    arr = []
+    for k,v in age_dict.items():
+        arr.append((k, v))
+    return arr
+
+
+# do not modify below this line
+print(get_dict_items({'Alice': 25, 'Bob': 30, 'Charlie': 35}))
+print(get_dict_items({'Alice': 25, 'Bob': 30, 'Charlie': 35, 'David': 40}))
+print(get_dict_items({'Bob': 30, 'David': 40, 'Charlie': 35, 'Alice': 25, 'Eve': 45}))
+print(get_dict_items({'Alice': 25, 'Bob': 30, 'Charlie': 35, 'David': 40, 'Eve': 45, 'Frank': 50}))
